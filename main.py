79046cd9ce40fb06b4be6b7a01b8a4d037b46456
Yaroslav Tsalko <yaroslav_tsalko@epam.com>
2024-07-28 17:21:08 +0300

    Fixed some issues, added convertation from bytes to mb and gb

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import psutil
 
 app = Flask(__name__)
 
-def get_system_metrix():
+
+def bytes_to_megabytes(bytes):
+    return int(bytes / (1024 * 1024))
+
+def bytes_to_gigabytes(bytes):
+    return int(bytes / (1024 * 1024 * 1024))
+
+def get_system_metrics():
     cpu_usage = psutil.cpu_percent(interval=1)
     mem_usage = psutil.virtual_memory()
     disk_usage = psutil.disk_usage('/')
@@ -11,25 +18,25 @@ def get_system_metrix():
 
     return {
         'cpu_usage': cpu_usage,
-        'memory_total': mem_usage.total,
-        'memory_used': mem_usage.used,
-        'memory_free': mem_usage.available,
-        'disk_total': disk_usage.total,
-        'disk_used': disk_usage.used,
-        'disk_free': disk_usage.free,
-        'net_sent': net_info.bytes_sent,
-        'net_recv': net_info.bytes_recv
+        'memory_total': bytes_to_megabytes(mem_usage.total),
+        'memory_used': bytes_to_megabytes(mem_usage.used),
+        'memory_free': bytes_to_megabytes(mem_usage.available),
+        'disk_total': bytes_to_gigabytes(disk_usage.total),
+        'disk_used': bytes_to_gigabytes(disk_usage.used),
+        'disk_free': bytes_to_gigabytes(disk_usage.free),
+        'net_sent': bytes_to_gigabytes(net_info.bytes_sent),
+        'net_recv': bytes_to_gigabytes(net_info.bytes_recv)
     }
 
 @app.route('/metrix', methods=["GET"])
 def metrics():
-    metrix = get_system_metrix()
+    metrix = get_system_metrics()
     return jsonify(metrix)
 
 @app.route('/')
 def index():
-    metrix = get_system_metrix()
-    return render_template('./index.html',metrics=metrics)
+    metrics = get_system_metrics()
+    return render_template('index.html', metrics=metrics)
 
 
 if __name__ == '__main__':
